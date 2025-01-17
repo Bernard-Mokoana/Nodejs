50593d81ea201c5129131e7159f04fbd2b3d06ab
Bernard <bernarddira@gmail.com>
2025-01-17 19:00:31 +0200

    "update express restful api with mongoDB database"

--- ExpressRestApi/server.js
+++ ExpressRestApi/server.js
@@ -1,68 +1,88 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
 const port = 3000;
 
 app.use(express.json());
 
-let users = [
-    { id: 1, name: "Bernard", email: "bernard.com" },
-    { id: 2, name: "Alfred", email: "alfred.com" }
-];
+mongoose.connect('mongodb+srv://Bernard:bernard12@cluster0.n19gr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
+.then(() => console.log('Connected to MongoDB Atlas'))
+.catch((err) => console.error('Error connecting to MongoDB Atlas:', err));
 
-
-app.get('/api/users', (req, res) => {
-    res.json(users);
-});
-
-app.post('/api/users', (req, res) => {
-    const newUser = {
-        id: users.length + 1,
-        name: req.body.name,
-        email: req.body.email
-    };
-    users.push(newUser);
-    res.status(201).json(newUser);
-});
-
-
-app.get('/api/users/:id', (req, res) => {
-    const userId = parseInt(req.params.id);
-    const user = users.find(u => u.id === userId);
-
-    if (!user) {
-        return res.status(404).json({ message: 'User not found' });
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
     }
-
-    res.json(user);
 });
+const User = mongoose.model('User', userSchema);
 
-
-app.put('/api/users/:id', (req, res) => {
-    const userId = parseInt(req.params.id);
-    const user = users.find(u => u.id === userId);
-
-    if (!user) {
-        return res.status(404).json({ message: 'User not found' });
+app.get('/api/users', async (req, res) => {
+    try{
+        const users = await User.find();
+        res.json(users);
+    } catch(error){
+        res.status(500).json({message: 'Error fetching users', error});
     }
-
-    user.name = req.body.name || user.name;
-    user.email = req.body.email || user.email;
-
-    res.json(user);
 });
-
-
-app.delete('/api/users/:id', (req, res) => {
-    const userId = parseInt(req.params.id);
-    const userIndex = users.findIndex(u => u.id === userId);
-
-    if (userIndex === -1) {
-        return res.status(404).json({ message: 'User not found' });
+ 
+app.post('/api/users', async (req, res) => {
+    try{
+        const newUser = new User({
+            name: req.body.name,
+            email: req.body.email
+        });
+        const savedUser = await newUser.save();
+        res.status(201).json(savedUser);
+    } catch(error) {
+        res.status(500).json({message: 'Error adding user', error});
     }
-
-    users.splice(userIndex, 1);
-    res.json({ message: 'User deleted' });
 });
+
+app.get('/api/users/:id', async (req, res) => {
+    try {
+        const user = await User.findById(req.params.id);
+
+        if(!user){
+            return res.status(404).json({message: 'User not found'});
+        }
+        res.json(user);
+    } catch (error) {
+        res.status(404).json({message: 'Error fetching user', error});
+    }
+});
+
+app.put('/api/users/:id', async (req, res) => {
+    try{
+        const updatedUser = await User.findByIdAndUpdate(req.params.id,
+            {name: req.body.name, email: req.body.email},
+            {new: true, runValidators: true});
+
+            if(!updatedUser){
+                return res.status(404).json({message: 'User not found'});
+            }
+            res.json(updatedUser);
+    } catch(error) {
+        res.status(500).json({message: "Error updating user", error});
+    }
+});
+
+app.delete('/api/users/:id', async (req, res) => {
+    try {
+        const deletedUser = await User.findByIdAndDelete(req.params.id);
+
+        if(!deletedUser){
+            return res.status(404).json({message: 'User not found'});
+        }
+    } catch (error) {
+        res.status(500).json({message: 'Error deleting user', error});
+    }
+})
 
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
